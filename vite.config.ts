041ebdb06f7688ec58fdbce45bb73bd97b823ec2
MG-Rashad <almejrabrashad@gmail.com>
2026-07-01 22:08:31 +0200

    fix: unlock Nitro to generate correct .output build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  // Change to "node" so Vercel can run it natively
+  nitro: { preset: "node" },
+  
   tanstackStart: {
     server: { entry: "server" },
   },
